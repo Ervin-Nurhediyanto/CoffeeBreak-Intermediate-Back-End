dfbb7af02f4fdad73f2fd383c835c8ee61e85a5d
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-08-30 05:30:12 +0700

    6th

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,13 +3,14 @@ const categoryController = require('../controllers/categories')
 const router = express.Router()
 const { verifyAccess, verifyAccessAdmin } = require('../middlewares/auth')
 const { upload } = require('../middlewares/multer')
-const redis = require('../middlewares/redis')
+// const redis = require('../middlewares/redis')
 
 router
   .get('/:id', verifyAccess, categoryController.getCategoryById)
-  .get('/', verifyAccess, redis.cacheGetAllCategory, categoryController.getAllcategory)
-  .post('/', verifyAccessAdmin, redis.clearGetAllCategory, upload, categoryController.insertCategory)
+  .get('/', verifyAccess, categoryController.getAllcategory)
+  .post('/', verifyAccessAdmin, upload, categoryController.insertCategory)
   .patch('/:id', verifyAccessAdmin, upload, categoryController.updateCategory)
   .delete('/:id', verifyAccessAdmin, categoryController.deleteCategory)
-
+// .get('/', verifyAccess, redis.cacheGetAllCategory, categoryController.getAllcategory)
+// .post('/', verifyAccessAdmin, redis.clearGetAllCategory, upload, categoryController.insertCategory)
 module.exports = router
--- src/routes/histories.js
+++ src/routes/histories.js
@@ -3,13 +3,15 @@ const historyController = require('../controllers/histories')
 const router = express.Router()
 const { verifyAccess, verifyAccessAdmin } = require('../middlewares/auth')
 const { upload } = require('../middlewares/multer')
-const redis = require('../middlewares/redis')
+// const redis = require('../middlewares/redis')
 
 router
   .get('/:id', verifyAccess, historyController.getHistoryById)
-  .get('/', verifyAccess, redis.cacheGetAllHistory, historyController.getAllhistory)
-  .post('/', verifyAccessAdmin, redis.clearGetAllHistory, upload, historyController.insertHistory)
+  .get('/', verifyAccess, historyController.getAllhistory)
+  .post('/', verifyAccessAdmin, upload, historyController.insertHistory)
   .patch('/:id', verifyAccessAdmin, upload, historyController.updateHistory)
   .delete('/:id', verifyAccessAdmin, historyController.deleteHistory)
+  // .get('/', verifyAccess, redis.cacheGetAllHistory, historyController.getAllhistory)
+  // .post('/', verifyAccessAdmin, redis.clearGetAllHistory, upload, historyController.insertHistory)
 
 module.exports = router
